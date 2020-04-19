c673961f255160dde27be6a877ba2c02253fc9ea
Lucasmouchague <lucas.mouchague@ynov.com>
2020-04-19 17:07:35 +0200

    some routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
